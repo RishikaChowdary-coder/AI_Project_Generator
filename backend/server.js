52cd2f154b4b04cb716e6d2be8a0fc129762af4a
RishikaChowdary <chowdary.rishika@gmail.com>
2026-06-21 20:12:03 +0530

    change

--- backend/server.js
+++ backend/server.js
@@ -2,29 +2,33 @@ require("dotenv").config();
 console.log("KEY:", process.env.GEMINI_API_KEY);
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
 
-const projectRoutes =
-require("./routes/projectRoutes");
+const projectRoutes = require("./routes/projectRoutes");
 
 const app = express();
-
-app.get("/", (req, res) => {
-  res.send("AI Project Generator Backend is Running!");
-});
 
 app.use(cors());
 app.use(express.json());
 
+// Serve frontend files
 app.use(
-    "/api/project",
-    projectRoutes
+  express.static(
+    path.join(__dirname, "../frontend")
+  )
 );
 
-const PORT =
-process.env.PORT || 5000;
+// Home page
+app.get("/", (req, res) => {
+  res.sendFile(
+    path.join(__dirname, "../frontend/index.html")
+  );
+});
+
+app.use("/api/project", projectRoutes);
+
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-    console.log(
-        `Server running on port ${PORT}`
-    );
+  console.log(`Server running on port ${PORT}`);
 });
